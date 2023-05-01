d048affa6366137bb6ce9616c0a716adf10ddb50
dendane-jalil <71212380+dendane-jalil@users.noreply.github.com>
2023-05-01 23:45:08 +0100

    first commit ever

---
+++ ecom-script.js
@@ -0,0 +1,5 @@
+// this is where the navigation bar urls routes are implemented , each page got a unique url.
+// to acces the page you click on the elemetnt
+const herobtn = document.getElementById("hero");
+herobtn.addEventListener("click", ()=>{ window.location.href='ecom-index.html'} );
+
---
+++ javascripttest.js
@@ -0,0 +1,23 @@
+const darkbtntoggel = document.getElementById("drkbtn");
+darkbtntoggel.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+})
+
+// const lightbtn = document.getElementById("newitem") //get the element first then make the operation
+// lightbtn.addEventListener("click", () => document.body.classList.toggle('light'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
