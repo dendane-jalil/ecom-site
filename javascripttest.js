18e1db2704ff9f78f7d0f6a6d52c6781837503ae
dendane-jalil <71212380+dendane-jalil@users.noreply.github.com>
2023-06-03 14:58:06 +0100

    added a loader to all pages

--- javascripttest.js
+++ javascripttest.js
@@ -6,18 +6,31 @@ darkbtntoggel.addEventListener("click", () => {
 // const lightbtn = document.getElementById("newitem") //get the element first then make the operation
 // lightbtn.addEventListener("click", () => document.body.classList.toggle('light'))
 
+initializing function
+    increase 
+decrease
+reset
+
+let count = 0;
+
+function decrease() {
+    count = count - 1
+    return count
+}
+function increase() {
+    count = count + 1;
+    return count
+}
+
+if count changed
+function count-UI - updater{
+    element.innerhtml = <p>`${count}`</p>
+}
+i need to keep refreshing each time the value is updated
 
 
 
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
 
-
-
-
-
-
-
-
-
-
-
-
+}
